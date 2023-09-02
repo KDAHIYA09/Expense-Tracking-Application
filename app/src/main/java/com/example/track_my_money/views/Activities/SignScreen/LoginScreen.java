c9a972cb9950c9490b92c94a85980144bc08da53
Harsh <kumarharsh74799gmail.com>
2023-09-02 16:25:17 +0530

    login screen Create

---
+++ app/src/main/java/com/example/track_my_money/views/Activities/SignScreen/LoginScreen.java
@@ -0,0 +1,16 @@
+package com.example.track_my_money.views.Activities.SignScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.track_my_money.R;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
--- app/src/main/java/com/example/track_my_money/views/Activities/SignScreen/SliderScreenActivity.java
+++ app/src/main/java/com/example/track_my_money/views/Activities/SignScreen/SliderScreenActivity.java
@@ -3,10 +3,12 @@ package com.example.track_my_money.views.Activities.SignScreen;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -56,8 +58,20 @@ public class SliderScreenActivity extends AppCompatActivity {
                     mSLideViewPager.setCurrentItem(getitem(1),true);
                 else {
 
-                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                    startActivity(i);
+                    Intent i = new Intent(getApplicationContext(),LoginScreen.class);
+
+
+                    Pair[] pairs = new Pair[1];
+                    pairs[0] = new Pair<View, String>(findViewById(R.id.nextbtn), "transition_login");
+
+
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SliderScreenActivity.this, pairs);
+                        startActivity(i, options.toBundle());
+                    } else {
+                        startActivity(i);
+                    }
+
                     finish();
 
                 }
@@ -71,7 +85,18 @@ public class SliderScreenActivity extends AppCompatActivity {
 
 
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+
+
+                Pair[] pairs = new Pair[1];
+                pairs[0] = new Pair<View, String>(findViewById(R.id.skipButton), "transition_login");
+
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SliderScreenActivity.this, pairs);
+                    startActivity(i, options.toBundle());
+                } else {
+                    startActivity(i);
+                }
                 finish();
 
             }
